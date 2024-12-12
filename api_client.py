76c3a0a5e5b02a58cb7ff8ea51515a0c14954860
girish332 <kulkarni.gi@northeastern.edu>
2024-12-12 17:01:23 -0500

    feat: RAG chatbot on warren buffet

---
+++ api_client.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+
+def call_rest_api():
+    # api_url = "https://openai-wb-demo.herokuapp.com/chat_all"
+    api_url = "http://127.0.0.1:5000/chat_all"
+    headers = {"Content-Type": "application/json"}
+    msg_list = [{'role': 'user', 'content': 'what do you think about bank failure'}]
+    response = requests.post(api_url, data=json.dumps(msg_list), headers=headers)
+    print(response.content)
+
+
+call_rest_api()
